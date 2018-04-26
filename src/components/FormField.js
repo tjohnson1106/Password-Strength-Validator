c3f21b23883aa7d04ab42ad3f1f038b1b236af9e
Thomas_Johnson <tjohnson1106@gmail.com>
2018-04-26 16:25:13 -0400

    created state variable and error catching

---
+++ src/components/FormField.js
@@ -0,0 +1,82 @@
+import React, { Component, Fragment } from "react";
+
+class FormField extends Component {
+  state = {
+    value: "",
+    dirty: false,
+    errors: []
+  };
+
+  hasChanged = e => {
+    e.preventDefault();
+
+    const {
+      label,
+      required = false,
+      validator = f => f,
+      onStateChanged = f => f
+    } = this.props;
+
+    const value = e.target.value;
+    const isEmpty = value.length === 0;
+    const requiredMissing = this.state.dirty && required && isEmpty;
+
+    let errors = [];
+
+    if (requiredMissing) {
+      // if required and is empty, add required error to state
+      errors = [...errors, `${label} is required`];
+    } else if ("function" === typeof validator) {
+      try {
+        validator(value);
+      } catch (e) {
+        errors = [...errors, e.message];
+      }
+    }
+    this.setState(
+      ({ dirty = false }) => ({ value, errors, dirty: !dirty || dirty }),
+      () => onStateChanged(this.state)
+    );
+  };
+
+  render() {
+    const { value, dirty, errors } = this.state;
+    const { type, label, fieldId, placeholder, children } = this.props;
+
+    const hasErrors = errors.length > 0;
+    const controlClass = [
+      "form-control",
+      dirty ? (hasErrors ? "is-invalid" : "is-invalid") : ""
+    ]
+      .join(" ")
+      .trim();
+
+    return (
+      <Fragment>
+        <div className="form-group px-3 pb-2">
+          <div className="d-flex flex-row justify-content-between align-items-center">
+            <label htmlFor={fieldId} className="control-label">
+              {label}
+            </label>
+            {hasErrors && (
+              <div className="error form-hint font-weight-bold text-right m-0 mb-2">
+                {errors[0]}
+              </div>
+            )}
+          </div>
+          {children}
+          <input
+            type={type}
+            className={controlClass}
+            id={fieldId}
+            placeholder={placeholder}
+            value={value}
+            onChange={this.hasChanged}
+          />
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default FormField;
