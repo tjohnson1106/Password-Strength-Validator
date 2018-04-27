99092927ce6b124d00cde7bbc1f12dd8ed3c6a20
Thomas_Johnson <tjohnson1106@gmail.com>
2018-04-27 16:50:41 -0400

    rendered variable and defined props

--- src/components/PasswordField.js
+++ src/components/PasswordField.js
@@ -24,7 +24,29 @@ class PasswordField extends Component {
       }, () => this.props.onStateChanged(state));
   };
 
+  validatePasswordStrong = (value) => {
+      if ( value.length <= this.thresholdLength ) throw new Error ("Password is too short");
+      if ( zxcvbn(value).score < this.minStrength ) throw new Error ("Password is too weak");
+  };
+
     render() {
+           
+         const { type, validator, onStateChanged, children, ...restProps } = this.props;
+         const { password, strength } = this.state;
+         
+         const passwordLength = password.length;
+         const passwordStrong = strength >=  this.minStrength;
+         const passwordLong = passwordLength > this.thresholdLength;
+
+         const counterClass = ['badge badge-pill', passwordLong 
+         ? passwordStrong ? 'badge-success' : 'badge-warning' : 'badge-danger'].join(' ').trim();
+        
+         const strengthClass = ['strength-meter mt-2', passwordLength > 0 
+         ? 'visible' : 'invisible'].join(' ').trim();
+
+
+
+
         return (
             
         );
